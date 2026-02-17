c2dc9211dbbb0852b8ee927f686849b375e5abab
unknown <qjuanpablo21@gmail.com>
2026-02-17 17:43:44 -0600

    Setup initial project structure

---
+++ main.py
@@ -0,0 +1,21 @@
+# +----------------------------------------------------------------------------+
+# | Authors.......: Juan Pablo Quezada Jimenez <qjuanpablo21@gmail.com>
+# | First release: February 11th, 2026
+# | Last update..: February 11th, 2026
+# | WhatIs.......: Blackjack Game - Main
+# +----------------------------------------------------------------------------++
+# ------------------------- Instructions -----------------------
+# TODO:
+
+# ------------ Resources / Documentation involved -------------
+# The Translator in Your Computer: https://cpu.land/the-translator-in-your-computer
+
+# ------------------------- Libraries -------------------------
+
+
+# ------------------------- Functions -------------------------
+
+
+# ------------------------- Variables -------------------------
+
+# --------------------------- Code ----------------------------
