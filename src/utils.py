0cdfad79ae2879e7db317b6c36b5c6e933d0cc32
unknown <qjuanpablo21@gmail.com>
2026-03-02 08:47:14 -0600

    Create menu in utilis.py

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,33 @@
+import os
 from art import text2art
+from InquirerPy import inquirer
 
-result=text2art("BLACKJACK")
-print(result)
+os.system('color')
 
+
+def main():
+    try:
+        result = text2art("BLACKJACK")
+        print(result)
+
+        choices = [
+            {"name": "[ START GAME ]", "value": "Start"},
+            {"name": "[  SETTINGS  ]", "value": "Settings"},
+            {"name": "[    EXIT    ]", "value": "Exit"},
+        ]
+
+        action = inquirer.select(
+            message="Select an action",
+            choices=choices,
+            pointer=">",
+            border=True
+        ).execute()
+
+        print(f"Already Selected: {action}")
+    except Exception as e:
+        print(f"Error de renderizado: {e}")
+        action = input("Select (Start/Settings/Exit): ")
+
+
+if __name__ == "__main__":
+    main()
