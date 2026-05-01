22bafd87fa5ba6d5d9f3c6513de75cff7a63781c
JuanPaQuezada <qjuanpablo21@gmail.com>
2026-04-30 23:04:37 -0600

    adding player class in player.py

--- src/player.py
+++ src/player.py
@@ -1,4 +1,26 @@
-#Holaaaaaaa
+#player class 
+class Player:
+    def __init__(self,name):
+        self.name=name
+        self.hand=[]
+    def add_card(self, card):
+        self.hand.append(card)
+    def calculate_score(self):
+        score=0
+        aces=0
+        for card in self.hand:
+            if card.rank in ["J","Q","K"]:
+                score+=10
+            elif card.rank=="A":
+                score+=11
+                aces+=1
+            else:
+                score+=int(card.rank)
 
-#nldbgfk
-#fdsknhdhjsfdsfdsndsjknkdasnk
+        while score>21 and aces>0:
+            score-=10
+            aces-=1
+
+        return score
+
+
